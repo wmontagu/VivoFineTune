cddc5e1349b3fbc0999926a749b67171d1ef6ea3
wmontagu <wmontagu@andrew.cmu.edu>
2024-08-08 01:19:28 -0500

    protecting personal info by not uploading direct messages, fixed gitignore and unsuccessfully copied things

--- delfiles.py
+++ delfiles.py
@@ -2,8 +2,10 @@ import os
 import pandas as pd
 import random
 import math
-def delChannels():
-    directory = 'messages'
+import json
+#downlaods messages wihtout emojis (emojis breaks things)
+
+def CreateFilterDiscord(directory, messagesList, file_path):
     for d in os.listdir(directory):
         subdir = os.path.join(directory, d)
         for filename in os.listdir(subdir):
@@ -11,13 +13,20 @@ def delChannels():
             if "channel" in path:
                 print(path)
                 os.remove(path)
+            else:
+                f = open(path, "r")
+                print(path)
+                try:
+                    lines = json.loads(f.read())   
+                except UnicodeDecodeError as e:
+                    print(f"Unicode decoding error: {e}")
+                messagesList.append(lines)
+                with open(file_path, "w") as file:
+                    file.write(lines)
+    print(messagesList)
+    print(f"File '{file_path}' created with specified content.")
 
-def filter(directory, date, messageLength):
-    messagesContents = []
-    for d in os.listdir(directory):
-        subdir = os.path.join(directory, d)
-        for filename in os.listdir(subdir):
-            path = os.path.join(subdir, filename)
-    return messages
-
-
+directory = 'messages'
+messagesList = []
+file_path = "allMessages.json"
+CreateFilterDiscord(directory, messagesList, file_path)
---
+++ main.py
@@ -0,0 +1,6 @@
+import delfiles.py
+directory = 'messages'
+messagesList = []
+file_path = "allMessages.json"
+
+CreateFilterDiscord(directory, messagesList, file_path)
